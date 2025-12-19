ebdd24fbdf739db0f375b21ffc789f756942386f
DemonicRavaGe <malektheangelking00@gmail.com>
2025-12-19 00:00:09 -0500

    Added initial files of the project

---
+++ src/client/java/net/demoravage/mcsttmod/MCSTTTranslationModClient.java
@@ -0,0 +1,10 @@
+package net.demoravage.mcsttmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCSTTTranslationModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
